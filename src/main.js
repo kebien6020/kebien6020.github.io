3eb237c30b6d1c1c10c9bf63333c85acad1ea9ed
Kevin Peña <kevin.pena.prog@gmail.com>
2017-03-20 14:12:59 -0500

    Style for desktop-sized screens

--- src/main.js
+++ src/main.js
@@ -1,1 +1,14 @@
 import './main.scss'
+
+// Smooth scrolling
+const $anchors = [
+  document.getElementById('projects'),
+  document.getElementById('about'),
+  document.getElementById('contact')
+]
+
+$anchors.forEach($anchor => {
+  $anchor.scrollIntoView({
+    behavior: 'smooth'
+  })
+})
